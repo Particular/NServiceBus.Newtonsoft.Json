ca410f8c6ced168a8024ceb9f122f3c93c8a97ed
Simon Cropp <simon.cropp@gmail.com>
2016-08-08 15:27:56 +1000

    add acceptance tests

---
+++ src/AcceptanceTests/SetupAcceptanceTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+/// <summary>
+/// Global setup fixture
+/// </summary>
+[SetUpFixture]
+public class SetupAcceptanceTests
+{
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+
+    }
+}
+
