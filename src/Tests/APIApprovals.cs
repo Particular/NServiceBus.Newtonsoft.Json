9fc7de89bbe6f75aedf509570431e6f6692e0224
Simon Cropp <simon.cropp@gmail.com>
2017-06-08 18:06:06 +1000

    remove redundant config

--- src/Tests/APIApprovals.cs
+++ src/Tests/APIApprovals.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
+﻿using System.IO;
 using System.Runtime.CompilerServices;
-using ApprovalTests;
+using ApiApprover;
+using NServiceBus;
 using NUnit.Framework;
-using PublicApiGenerator;
 
 [TestFixture]
 public class APIApprovals
@@ -14,21 +11,7 @@ public class APIApprovals
     [MethodImpl(MethodImplOptions.NoInlining)]
     public void Approve()
     {
-        var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Newtonsoft.Json.dll");
-        var assembly = Assembly.LoadFile(combine);
-        var publicApi = Filter(ApiGenerator.GeneratePublicApi(assembly));
-        Approvals.Verify(publicApi);
+        Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+        PublicApiApprover.ApprovePublicApi(typeof(NewtonsoftSerializer).Assembly);
     }
-
-    string Filter(string text)
-    {
-        return string.Join(Environment.NewLine, text.Split(new[]
-        {
-            Environment.NewLine
-        }, StringSplitOptions.RemoveEmptyEntries)
-            .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
-            .Where(l => !string.IsNullOrWhiteSpace(l))
-            );
-    }
-
 }
