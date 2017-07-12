0312b0afd9c8f3164842bb561c84515469c604d8
danielmarbach <daniel.marbach@openplace.net>
2017-07-12 22:36:10 +0200

    Test to verify that when subtype is not passed to message types it can still be deserialized (in case with unobtrusive when you only handle the base type)

---
+++ src/Tests/With_base_but_without_concrete.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Linq;
+using NServiceBus;
+using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
+using NServiceBus.Newtonsoft.Json;
+using NUnit.Framework;
+
+[TestFixture]
+public class With_base_type_but_without_sub_type_in_message_types
+{
+    [Test]
+    public void Deserialize()
+    {
+        var messageMapper = new MessageMapper();
+        var serializer = new JsonMessageSerializer(messageMapper, null, null, null, null);
+        var messageTypes = new[]
+        {
+            typeof(ISomeBaseMessage)
+        };
+        messageMapper.Initialize(messageTypes.Union(new[]
+        {
+            typeof(ISomeMessage)
+        }));
+        var message = messageMapper.CreateInstance<ISomeMessage>(x => x.SomeProperty = "test");
+        using (var stream = new MemoryStream())
+        {
+            serializer.Serialize(message, stream);
+
+            stream.Position = 0;
+            var result = (ISomeBaseMessage)serializer.Deserialize(stream, messageTypes)[0];
+
+            Assert.AreEqual("test", result.SomeProperty);
+        }
+
+    }
+
+    public interface ISomeMessage : ISomeBaseMessage
+    {
+    }
+
+    public interface ISomeBaseMessage : IMessage
+    {
+        string SomeProperty { get; set; }
+    }
+}
