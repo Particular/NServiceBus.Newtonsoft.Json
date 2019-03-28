a65bcc6884082830c527276b3f871be6ac2126f4
Jimmy Bogard <jimmy.bogard@gmail.com>
2019-03-28 16:17:52 -0500

    Changing default setting of encoding to not include the BOM for UTF8 per JSON RFC; fixes #53

---
+++ src/Tests/With_UTF8_BOM.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text;
+using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
+using NServiceBus.Newtonsoft.Json;
+using NUnit.Framework;
+
+[TestFixture]
+public class With_UTF8_BOM
+{
+
+    [Test]
+    public void Run()
+    {
+        var messageMapper = new MessageMapper();
+        var serializer = new JsonMessageSerializer(messageMapper, null, null, null, null);
+
+        var serialized = new UTF8Encoding(true).GetBytes($"{{\"{nameof(SimpleMessage.SomeProperty)}\":\"John\"}}");
+
+        using (var stream = new MemoryStream(serialized))
+        {
+            stream.Position = 0;
+
+            var result = (SimpleMessage)serializer.Deserialize(stream, new[] { typeof(SimpleMessage) })[0];
+
+            Assert.AreEqual("John", result.SomeProperty);
+        }
+    }
+    public class SimpleMessage
+    {
+        public string SomeProperty { get; set; }
+    }
+
+}
