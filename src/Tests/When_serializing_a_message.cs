e53669c84675194c48c5449389ba42a114046e34
Sean Feldman <feldman.sean@gmail.com>
2019-05-21 14:44:30 -0600

    Undo BOM changes
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/Tests/When_serializing_a_message.cs
+++ src/Tests/When_serializing_a_message.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
+using Newtonsoft.Json;
 using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
 using NServiceBus.Newtonsoft.Json;
 using NUnit.Framework;
@@ -7,12 +9,44 @@ using NUnit.Framework;
 [TestFixture]
 public class When_serializing_a_message
 {
-
     [Test]
-    public void Should_not_emit_UTF8_BOM()
+    public void Should_emit_UTF8_BOM_by_default()
     {
         var messageMapper = new MessageMapper();
         var serializer = new JsonMessageSerializer(messageMapper, null, null, null, null);
+        var message = new SimpleMessage();
+        using (var stream = new MemoryStream())
+        {
+            serializer.Serialize(message, stream);
+
+            stream.Position = 0;
+
+            var result = stream.ToArray();
+            var utf8bom = new UTF8Encoding(true).GetPreamble();
+
+            for (var i = 0; i < utf8bom.Length; i++)
+            {
+                Assert.AreEqual(utf8bom[i], result[i]);
+            }
+        }
+    }
+    
+    [Test]
+    public void Should_not_emit_UTF8_BOM_if_configured_not_to()
+    {
+        var messageMapper = new MessageMapper();
+
+        Func<Stream, JsonWriter> writerCreator = stream =>
+        {
+            var streamWriter = new StreamWriter(stream, new UTF8Encoding(false, false));
+            return new JsonTextWriter(streamWriter)
+            {
+                Formatting = Formatting.None
+            };
+        };
+
+        var serializer = new JsonMessageSerializer(messageMapper, null, writerCreator, null, null);
+        
         var message = new SimpleMessage();
         using (var stream = new MemoryStream())
         {
@@ -29,6 +63,7 @@ public class When_serializing_a_message
             }
         }
     }
+
     public class SimpleMessage
     {
         public string SomeProperty { get; set; }
