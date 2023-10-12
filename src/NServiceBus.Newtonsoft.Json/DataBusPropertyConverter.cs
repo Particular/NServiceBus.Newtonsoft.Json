c891b69ce2e3c6dab4d39a5736a734028fbec8d6
Brandon Ording <bording@gmail.com>
2023-10-12 19:19:11 -0400

    Add JsonConverter for DataBusProperty

---
+++ src/NServiceBus.Newtonsoft.Json/DataBusPropertyConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.Newtonsoft.Json
+{
+    using System;
+    using global::Newtonsoft.Json;
+
+    class DataBusPropertyConverter : JsonConverter<IDataBusProperty>
+    {
+        public override void WriteJson(JsonWriter writer, IDataBusProperty value, JsonSerializer serializer)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName(nameof(value.Key));
+            writer.WriteValue(value.Key);
+            writer.WritePropertyName(nameof(value.HasValue));
+            writer.WriteValue(value.HasValue);
+            writer.WriteEndObject();
+        }
+
+        public override IDataBusProperty ReadJson(JsonReader reader, Type objectType, IDataBusProperty existingValue, bool hasExistingValue, JsonSerializer serializer) => throw new NotImplementedException();
+
+        public override bool CanRead => false;
+    }
+}
